705d8f8b85f63a9998579f3426ea21b4641593d5
dwai1714 <dc@bwsdev.localdomain>
2024-03-19 16:06:10 +0530

    added a manual pipeline

---
+++ cicd/manual_pipeline.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import os
+import time
+from typing import Optional
+
+import click
+from humanfriendly import format_timespan
+from pyfiglet import Figlet
+
+from cicd import common_functions
+
+f = Figlet(font='slant')
+
+
+@click.command()
+@click.argument('file_name', type=str, required=True)
+@click.argument('versions_path', type=str, required=True)
+@click.option('--rollback', is_flag=True, help='Set this flag to perform rollback')
+def cli(versions_path: str, file_name: str, rollback: Optional[str]):
+    env = os.environ.get('ENV', 'DEV')
+
+    click.echo(f.renderText('run pipeline'))
+    click.echo(f'file_name: {file_name}')
+    click.echo(f'versions_path: {versions_path}')
+    click.echo(f'rollback: {rollback}')
+    click.echo()
+
+    if env != 'DEV':
+        raise ValueError('This command requires environment to be DEV only')
+    if rollback:
+        common_functions.apply_rollback(versions_path, file_name)
+    else:
+        common_functions.apply_forward(versions_path, file_name)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    cli()
+    click.echo(f'Execution time: {format_timespan(time.time() - start)}')
